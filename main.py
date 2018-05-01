0634599d05f7cf20fe7838628f6d12370384386e
Justin Serowik <JSerowik@protonmail.com>
2018-05-01 02:21:36 -0400

    Added Diet Options
    Added selection options for the diets available with variable constraints.

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import numpy
 
 # Ask user for inputs to determine ideal Daily Calorie Intake (DCI)
 std = input('Do you use the Metric system or Imperial? (M or I) ')
-if(std == "M"):
+if(std == 'M'):
 	weight = float(input('What is you weight in kilograms? '))
 	height = float(input('What is you height in meters? '))
-elif(std == "I"):
+elif(std == 'I'):
 	wlb = float(input('What is you weight in pounds? '))
 	weight = wlb/2.2046226218
 	print('Input your height: ')
@@ -14,36 +14,53 @@ elif(std == "I"):
 	h_inch = int(input('Inches: '))
 	h_inch += h_ft * 12
 	height = (h_inch * 2.54)/100
-age = float(input("How old are you? "))
+age = float(input('How old are you? '))
 sex = input('What is your sex? (M or F) ')
 if(sex == "M"):
 	DCI = 864 - 9.72 * age + (14.2 * weight + 503 * height)
 elif(sex == "F"):
 	DCI = 387 - 7.31 * age + (10.9 * weight + 660.7 * height)
-print("Metric weight is: " +str(round(weight,2)) + " kg")
+print('Metric weight is: ' +str(round(weight,2)) + ' kg')
 print('Metric height is: ' +str(round(height,2)) + ' m')
-print("Daily Caloric intake: "+str(round(DCI,2))+" calories")
-
-# Problem data.
-prot = 4
-carb = 4
-fat = 9
+print('Daily Caloric intake: '+str(round(DCI,2))+' calories')
 
 # Construct the problem.
 p = Variable()
 c = Variable()
 f = Variable()
 
-objective = Minimize((4*p)+(4*c)+(9*f))
-constraints = [p > 0, c > 0, f > 0, (4*p)+(4*c)+(9*f) >= DCI]
+diet = input('What diet would you like to explore? (Keto = K, Atkins = A, Zone = Z, Paleo = P) ')
+if(diet == 'K'):
+	prot = (DCI*0.20)
+	carb = (DCI*0.05)
+	fat = (DCI*0.75)
+	constraints = [p > 0, c > 0, (4*p)+(4*c)+(9*f) <= DCI, 4*p <= prot, 4*c <= carb, 9*f >= fat]
+elif(diet == 'A'):
+	constraints = [p >= 120, c > 0, f >= 120, (4*p)+(4*c)+(9*f) <= DCI, p <= 140, c <= 20, f <= 140]
+elif(diet == 'Z'):
+	prot = (DCI*0.3)
+	carb = (DCI*0.45)
+	fat = (DCI*0.3)
+	constraints = [p > 0, 4*c >= (DCI*0.35), (4*p)+(4*c)+(9*f) <= DCI, 4*p <= prot, 4*c <= carb, 9*f >= fat]
+elif(diet == 'P'):
+	constraints = [p >= 120, c >= 50, f > 0, (4*p)+(4*c)+(9*f) <= DCI, p <= 150, c <= 100]
+else:
+	constraints = [p > 0, c >0, f > 0, (4*p)+(4*c)+(9*f) <= DCI]
+# Problem data.
+#prot = 4
+#carb = 4
+#fat = 9
+
+objective = Maximize((4*p)+(4*c)+(9*f))
+#constraints = [p > 0, c > 0, f > 0, (4*p)+(4*c)+(9*f) <= DCI, 4*p <= prot_k, 4*c <= carb_k, 9*f >= fat_k]
 prob = Problem(objective, constraints)
 
 # The optimal objective is returned by prob.solve().
 result = prob.solve()
-print("{}: {} calories daily".format("Minimized intake", result))
-print("{}: {} grams".format("Protein", round(p.value,2)))
-print("{}: {} grams".format("Carbs", round(c.value,2)))
-print("{}: {} grams".format("Fat", round(f.value,2)))
+print('{}: {} calories daily'.format('Maximized intake', result))
+print('{}: {} grams'.format('Protein', round(p.value,2)))
+print('{}: {} grams'.format('Carbs', round(c.value,2)))
+print('{}: {} grams'.format('Fat', round(f.value,2)))
 # The optimal Lagrange multiplier for a constraint
 # is stored in constraint.dual_value.
 #print(constraints[0].dual_value)
